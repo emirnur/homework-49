fee2f791b1553a856e219ba3e733e45894af13ba
emirnur <nur.emirlan@gmail.com>
2019-09-26 19:34:37 +0600

    add migrations and dump

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from webapp.models import TrackerIssue, Type, Status
 
-# Register your models here.
+
+class TrackerIssueAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'summary', 'description', 'status', 'type', 'created_at']
+    list_filter = ['summary', 'status']
+    list_display_links = ['pk', 'summary']
+    search_fields = ['summary', 'description']
+    exclude = []
+    readonly_fields = ['created_at']
+
+
+admin.site.register(TrackerIssue, TrackerIssueAdmin)
+admin.site.register(Type)
+admin.site.register(Status)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class TrackerIssue(models.Model):
+    summary = models.CharField(max_length=200, null=False, blank=False, verbose_name='Заголовок')
+    description = models.TextField(max_length=3000, null=True, blank=True, verbose_name='Описание')
+    status = models.ForeignKey('Status', on_delete=models.PROTECT, null=True, blank=True, verbose_name='Статус',
+                                 related_name='issues')
+    type = models.ForeignKey('Type', on_delete=models.PROTECT, null=True, blank=True, verbose_name='Тип',
+                               related_name='issues')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+
+    def __str__(self):
+        return self.summary
+
+
+class Status(models.Model):
+    name = models.CharField(max_length=20, verbose_name='Статус')
+
+    class Meta:
+        verbose_name = 'Status'
+        verbose_name_plural = 'Statuses'
+
+    def __str__(self):
+        return self.name
+
+
+class Type(models.Model):
+    name = models.CharField(max_length=20, verbose_name='Тип')
+
+    def __str__(self):
+        return self.name
