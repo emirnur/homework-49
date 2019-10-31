f3325d5daec1ca94b0dc9095328a3cf041f34667
emirnur <nur.emirlan@gmail.com>
2019-10-31 17:23:52 +0600

    add function for enter to user account, list of user accounts

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
-from accounts.views import login_view, logout_view, register_view, user_activate_view
+from accounts.views import login_view, logout_view, register_view, user_activate_view, UserDetailView, UserChangeView, \
+    UserChangePasswordView, ListUserView
 
 urlpatterns = [
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
     path('register/', register_view, name='register'),
-    path('activate/<token>/', user_activate_view, name='user_activate')
+    path('activate/<token>/', user_activate_view, name='user_activate'),
+    path('profile/<pk>/', UserDetailView.as_view(), name='user_detail'),
+    path('profile/<pk>/edit/', UserChangeView.as_view(), name='user_update'),
+    path('profile/<pk>/change-password/', UserChangePasswordView.as_view(), name='user_change_password'),
+    path('users/list/', ListUserView.as_view(), name='user_list'),
 ]
 
 app_name = 'accounts'
--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,9 +1,12 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.mixins import UserPassesTestMixin
 from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
+from django.views.generic import DetailView, UpdateView, ListView
 
-from accounts.forms import SignUpForm
+from accounts.forms import SignUpForm, UserChangeForm, UserChangePasswordForm
 from accounts.models import Token
 from main.settings import HOST_NAME
 
@@ -70,3 +73,46 @@ def user_activate_view(request, token):
     user.save()
     login(request, user)
     return redirect('webapp:index')
+
+
+class UserDetailView(DetailView):
+    model = User
+    template_name = 'user_detail.html'
+    context_object_name = 'user_obj'
+
+
+class UserChangeView(UserPassesTestMixin, UpdateView):
+    model = User
+    template_name = 'user_update.html'
+    context_object_name = 'user_obj'
+    form_class = UserChangeForm
+
+    def test_func(self):
+        return self.get_object() == self.request.user
+
+    def get_success_url(self):
+        return reverse('accounts:user_detail', kwargs={'pk': self.object.pk})
+
+
+class UserChangePasswordView(UserPassesTestMixin, UpdateView):
+    model = User
+    template_name = 'user_change_password.html'
+    form_class = UserChangePasswordForm
+    context_object_name = 'user_obj'
+
+    def test_func(self):
+        return self.get_object() == self.request.user
+
+    # def form_valid(self, form):
+    #     user = form.save()
+    #     login(self.request, user)
+    #     return HttpResponseRedirect(self.get_success_url())
+
+    def get_success_url(self):
+        return reverse('accounts:login')
+
+
+class ListUserView(ListView):
+    template_name = 'user_list.html'
+    context_object_name = 'user_objs'
+    model = User
