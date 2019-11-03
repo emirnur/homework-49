69bf12f76e4a0d2712b55672646432b9a3bb82ff
emirnur <nur.emirlan@gmail.com>
2019-11-03 23:54:31 +0600

    Add model Profile for account

--- source/accounts/models.py
+++ source/accounts/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 from uuid import uuid4
 
@@ -9,3 +10,19 @@ class Token(models.Model):
 
     def __str__(self):
         return str(self.token)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, related_name='profile', on_delete=models.CASCADE,
+                                verbose_name='Пользователь')
+    avatar = models.ImageField(null=True, blank=True,
+                               upload_to='user_pics', verbose_name='Аватар')
+    description = models.TextField(null=True, blank= True, verbose_name='О себе')
+    github = models.URLField(null=True, blank=True, verbose_name='GitHub')
+
+    def __str__(self):
+        return self.user.get_full_name() + "'s Profile"
+
+    class Meta:
+        verbose_name = 'Профиль'
+        verbose_name_plural = 'Профили'
